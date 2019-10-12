eab1fc3c1520fdcb2eb80c5624dc1f9a3f8ee9da
mrminht2000 <mrminht2000@gmail.com>
2019-10-12 11:12:48 +0700

    add libraries

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,35 @@
+import org.lwjgl.*;
+import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+import org.lwjgl.system.*;
+
+import java.nio.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+
+
 public class MainWindow {
-    
+    private long window;
+
+    public void run() {
+
+        System.out.print("Game Start!");
+
+        init();
+        loop();
+
+
+    }
+
+    public void init() {}
+
+    public void loop() {
+    }
+
+
+
 }

8cf936886aba240c2cc7e1f840fea171c2cfecc9
mrminht2000 <mrminht2000@gmail.com>
2019-10-15 17:27:08 +0700

    Change something CHANGE_LOG GUI and myTexture class resource files update library

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,35 +1,9 @@
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
-
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
+import java.io.FileNotFoundException;
 
 public class MainWindow {
-    private long window;
 
-    public void run() {
-
-        System.out.print("Game Start!");
-
-        init();
-        loop();
-
-
+    public static void main(String[] args) throws FileNotFoundException {
+        new GUI().run();
     }
-
-    public void init() {}
-
-    public void loop() {
-    }
-
-
 
 }
